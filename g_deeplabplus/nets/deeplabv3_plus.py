22df0ffbbd499476040505cfc9baec9bbff09143
920581673 <102811257+920581673@users.noreply.github.com>
2022-06-29 18:11:22 +0800

    Add files via upload

---
+++ g_deeplabplus/nets/deeplabv3_plus.py
@@ -0,0 +1,345 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from nets.xception import xception
+from nets.mobilenetv2 import mobilenetv2
+import math
+
+class MobileNetV2(nn.Module):
+    def __init__(self, downsample_factor=16, pretrained=True):
+        super(MobileNetV2, self).__init__()
+        from functools import partial
+        
+        model           = mobilenetv2(pretrained)
+        self.features   = model.features[:-1]
+
+        self.total_idx  = len(self.features)
+        self.down_idx   = [2, 4, 7, 14]
+
+        if downsample_factor == 8:
+            for i in range(self.down_idx[-2], self.down_idx[-1]):   #【7,14】
+                self.features[i].apply(
+                    partial(self._nostride_dilate, dilate=2)   #在functools模块中有一个工具partial()，可以用来"冻结"一个函数的参数，并返回"冻结"参数后的新函数，，，空洞率为2
+                )
+            for i in range(self.down_idx[-1], self.total_idx):   #【7，len(self.features)】
+                self.features[i].apply(
+                    partial(self._nostride_dilate, dilate=4)
+                )
+        elif downsample_factor == 16:
+            for i in range(self.down_idx[-1], self.total_idx):
+                self.features[i].apply(
+                    partial(self._nostride_dilate, dilate=2)
+                )
+        
+    def _nostride_dilate(self, m, dilate):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            if m.stride == (2, 2):
+                m.stride = (1, 1)
+                if m.kernel_size == (3, 3):
+                    m.dilation = (dilate//2, dilate//2)
+                    m.padding = (dilate//2, dilate//2)
+            else:
+                if m.kernel_size == (3, 3):
+                    m.dilation = (dilate, dilate)
+                    m.padding = (dilate, dilate)
+
+    def forward(self, x):
+        low_level_features = self.features[:4](x)
+        x = self.features[4:](low_level_features)
+        return low_level_features, x 
+
+#----------------------------------------------#
+class _DenseASPPConv(nn.Sequential):
+    def __init__(self, in_channels, inter_channels, out_channels, atrous_rate,
+                 drop_rate=0.1, norm_layer=nn.BatchNorm2d, norm_kwargs=None):
+        super(_DenseASPPConv, self).__init__()
+        self.add_module('conv1', nn.Conv2d(in_channels, inter_channels, 1)),
+        self.add_module('bn1', norm_layer(inter_channels, **({} if norm_kwargs is None else norm_kwargs))),
+        self.add_module('relu1', nn.ReLU(True)),
+        self.add_module('conv2', nn.Conv2d(inter_channels, out_channels, 3, dilation=atrous_rate, padding=atrous_rate)),
+        self.add_module('bn2', norm_layer(out_channels, **({} if norm_kwargs is None else norm_kwargs))),
+        self.add_module('relu2', nn.ReLU(True)),
+        self.drop_rate = drop_rate
+
+    def forward(self, x):
+        features = super(_DenseASPPConv, self).forward(x)
+        if self.drop_rate > 0:
+            features = F.dropout(features, p=self.drop_rate, training=self.training)
+        return features
+
+
+class _DenseASPPBlock(nn.Module):
+    def __init__(self, in_channels, inter_channels1, inter_channels2,
+                 norm_layer=nn.BatchNorm2d, norm_kwargs=None):
+        super(_DenseASPPBlock, self).__init__()
+        self.aspp_3 = _DenseASPPConv(in_channels, inter_channels1, inter_channels2, 3, 0.1,
+                                     norm_layer, norm_kwargs)
+        self.aspp_6 = _DenseASPPConv(in_channels + inter_channels2 * 1, inter_channels1, inter_channels2, 6, 0.1,
+                                     norm_layer, norm_kwargs)
+        self.aspp_12 = _DenseASPPConv(in_channels + inter_channels2 * 2, inter_channels1, inter_channels2, 12, 0.1,
+                                      norm_layer, norm_kwargs)
+        self.aspp_18 = _DenseASPPConv(in_channels + inter_channels2 * 3, inter_channels1, inter_channels2, 18, 0.1,
+                                      norm_layer, norm_kwargs)
+        self.aspp_24 = _DenseASPPConv(in_channels + inter_channels2 * 4, inter_channels1, inter_channels2, 24, 0.1,
+                                      norm_layer, norm_kwargs)
+
+    def forward(self, x):
+        aspp3 = self.aspp_3(x)
+        x = torch.cat([aspp3, x], dim=1)
+
+        aspp6 = self.aspp_6(x)
+        x = torch.cat([aspp6, x], dim=1)
+
+        aspp12 = self.aspp_12(x)
+        x = torch.cat([aspp12, x], dim=1)
+
+        aspp18 = self.aspp_18(x)
+        x = torch.cat([aspp18, x], dim=1)
+
+        aspp24 = self.aspp_24(x)
+        x = torch.cat([aspp24, x], dim=1)
+
+        return x
+
+
+class DenseASPP(nn.Module):
+    def __init__(self, in_dims, out_dims, rate=[6, 12, 18]):
+        super(DenseASPP, self).__init__()
+
+        self.aspp_block1 = nn.Sequential(
+            nn.Conv2d(
+                in_dims, in_dims, 3, stride=1, padding=rate[0], dilation=rate[0]
+            ),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(in_dims),
+        )
+        self.aspp_block2 = nn.Sequential(
+            nn.Conv2d(
+                2 * in_dims, in_dims, 3, stride=1, padding=rate[1], dilation=rate[1]
+            ),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(in_dims),
+        )
+        self.aspp_block3 = nn.Sequential(
+            nn.Conv2d(
+                3 * in_dims, in_dims, 3, stride=1, padding=rate[2], dilation=rate[2]
+            ),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(in_dims),
+        )
+
+        self.output = nn.Conv2d(4 * in_dims, out_dims, 1)
+
+
+    def forward(self, x):
+        x1 = self.aspp_block1(x)
+        x2_in = torch.cat([x1, x], dim=1)
+
+        # 2_in_dim
+
+        x2 = self.aspp_block2(x2_in)
+
+        x3_in = torch.cat([x2, x2_in], dim=1)
+
+        # 3_in_dim
+
+        x3 = self.aspp_block3(x3_in)
+        out = torch.cat([x3_in, x3], dim=1)
+
+        # 4_in_dim
+
+        return self.output(out)
+#-----------------------------------------#
+#   ASPP特征提取模块
+#   利用不同膨胀率的膨胀卷积进行特征提取
+#-----------------------------------------#
+class ASPP(nn.Module):
+	def __init__(self, dim_in, dim_out, rate=1, bn_mom=0.1):
+		super(ASPP, self).__init__()
+		self.branch1 = nn.Sequential(
+				nn.Conv2d(dim_in, dim_out, 1, 1, padding=0, dilation=rate,bias=True),
+				nn.BatchNorm2d(dim_out, momentum=bn_mom),
+				nn.ReLU(inplace=True),
+		)
+		self.branch2 = nn.Sequential(
+				nn.Conv2d(dim_in, dim_out, 3, 1, padding=6*rate, dilation=6*rate, bias=True),
+				nn.BatchNorm2d(dim_out, momentum=bn_mom),
+				nn.ReLU(inplace=True),	
+		)
+		self.branch3 = nn.Sequential(
+				nn.Conv2d(dim_in, dim_out, 3, 1, padding=12*rate, dilation=12*rate, bias=True),
+				nn.BatchNorm2d(dim_out, momentum=bn_mom),
+				nn.ReLU(inplace=True),	
+		)
+		self.branch4 = nn.Sequential(
+				nn.Conv2d(dim_in, dim_out, 3, 1, padding=18*rate, dilation=18*rate, bias=True),
+				nn.BatchNorm2d(dim_out, momentum=bn_mom),
+				nn.ReLU(inplace=True),	
+		)
+		self.branch5_conv = nn.Conv2d(dim_in, dim_out, 1, 1, 0,bias=True)
+		self.branch5_bn = nn.BatchNorm2d(dim_out, momentum=bn_mom)
+		self.branch5_relu = nn.ReLU(inplace=True)
+
+		self.conv_cat = nn.Sequential(
+				nn.Conv2d(dim_out*5, dim_out, 1, 1, padding=0,bias=True),
+				nn.BatchNorm2d(dim_out, momentum=bn_mom),
+				nn.ReLU(inplace=True),		
+		)
+
+	def forward(self, x):
+		[b, c, row, col] = x.size()
+        #-----------------------------------------#
+        #   一共五个分支
+        #-----------------------------------------#
+		conv1x1 = self.branch1(x)
+		conv3x3_1 = self.branch2(x)
+		conv3x3_2 = self.branch3(x)
+		conv3x3_3 = self.branch4(x)
+        #-----------------------------------------#
+        #   第五个分支，全局平均池化+卷积
+        #-----------------------------------------#
+		global_feature = torch.mean(x,2,True)
+		global_feature = torch.mean(global_feature,3,True)
+		global_feature = self.branch5_conv(global_feature)
+		global_feature = self.branch5_bn(global_feature)
+		global_feature = self.branch5_relu(global_feature)
+		global_feature = F.interpolate(global_feature, (row, col), None, 'bilinear', True)
+		
+        #-----------------------------------------#
+        #   将五个分支的内容堆叠起来
+        #   然后1x1卷积整合特征。
+        #-----------------------------------------#
+		feature_cat = torch.cat([conv1x1, conv3x3_1, conv3x3_2, conv3x3_3, global_feature], dim=1)
+		result = self.conv_cat(feature_cat)
+		return result
+
+#---------------------------------------#
+#     开始定义CBAM模块
+#---------------------------------------#
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=8):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        # 利用1x1卷积代替全连接
+        self.fc1   = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        out1 = torch.cat([avg_out, max_out], dim=1)
+        out1 = self.conv1(out1)
+        return self.sigmoid(out1)
+
+class cbam_block(nn.Module):
+    def __init__(self, channel, ratio=8, kernel_size=7):
+        super(cbam_block, self).__init__()
+        self.channelattention = ChannelAttention(channel, ratio=ratio)
+        self.spatialattention = SpatialAttention(kernel_size=kernel_size)
+
+    def forward(self, x):
+        x1 = x*self.channelattention(x)
+        x = x + x1
+        x = x*self.spatialattention(x)
+        return x
+
+
+class DeepLab(nn.Module):
+    def __init__(self, num_classes, backbone="mobilenet", pretrained=True, downsample_factor=16):
+        super(DeepLab, self).__init__()
+        if backbone=="xception":
+            #----------------------------------#
+            #   获得两个特征层
+            #   浅层特征    [128,128,256]
+            #   主干部分    [30,30,2048]
+            #----------------------------------#
+            self.backbone = xception(downsample_factor=downsample_factor, pretrained=pretrained)
+            in_channels = 2048
+            low_level_channels = 256
+        elif backbone=="mobilenet":
+            #----------------------------------#
+            #   获得两个特征层
+            #   浅层特征    [128,128,24]
+            #   主干部分    [30,30,320]
+            #----------------------------------#
+            self.backbone = MobileNetV2(downsample_factor=downsample_factor, pretrained=pretrained)
+            in_channels = 320                        # ---------------------------------------------------------这里这里
+            low_level_channels = 24
+        else:
+            raise ValueError('Unsupported backbone - `{}`, Use mobilenet, xception.'.format(backbone))
+
+        #-----------------------------------------#
+        #   cbam
+        #   ASPP特征提取模块
+        #   利用不同膨胀率的膨胀卷积进行特征提取
+        #-----------------------------------------#
+        self.cbam = cbam_block(channel = in_channels)
+        self.aspp = DenseASPP(in_dims = in_channels, out_dims = 256,)
+        #----------------------------------#
+        #   浅层特征边
+        #----------------------------------#
+        self.shortcut_conv = nn.Sequential(
+            nn.Conv2d(low_level_channels, 48, 1),
+            nn.BatchNorm2d(48),
+            nn.ReLU(inplace=True)
+        )		
+
+        self.cat_conv = nn.Sequential(
+            nn.Conv2d(48+256, 256, 3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
+            nn.Conv2d(256, 256, 3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+
+            nn.Dropout(0.1),
+        )
+        self.cls_conv = nn.Conv2d(256, num_classes, 1, stride=1)
+        self.one_conv = nn.Conv2d(576, 256, 1, stride=1)
+    def forward(self, x):
+        H, W = x.size(2), x.size(3)
+        #-----------------------------------------#
+        #   获得两个特征层
+        #   low_level_features: 浅层特征-进行卷积处理
+        #   x : 主干部分-利用ASPP结构进行加强特征提取
+        #-----------------------------------------#
+        low_level_features, x = self.backbone(x)
+        x2 = self.cbam(x)
+        x = self.aspp(x)
+        x = torch.cat((x, x2), dim=1)
+        x = self.one_conv(x)
+        low_level_features = self.shortcut_conv(low_level_features)
+        
+        #-----------------------------------------#
+        #   将加强特征边上采样
+        #   与浅层特征堆叠后利用卷积进行特征提取
+        #-----------------------------------------#
+        x = F.interpolate(x, size=(low_level_features.size(2), low_level_features.size(3)), mode='bilinear', align_corners=True)   #双线性插值进行上采样
+        x = self.cat_conv(torch.cat((x, low_level_features), dim=1))
+        x = self.cls_conv(x)
+        x = F.interpolate(x, size=(H, W), mode='bilinear', align_corners=True)
+        return x
+
